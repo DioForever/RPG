d0837856c6b0dbe2ce685fac76bc44a98aa4dee4
DioForever <dioforever001@gmail.com>
2022-05-07 21:02:49 +0200

    Initial commit

---
+++ src/main/java/me/dioforever/rpg/Customs/Entities/Cryomancer.java
@@ -0,0 +1,25 @@
+package me.dioforever.rpg.Customs.Entities;
+
+import org.bukkit.Location;
+
+public class Cryomancer {
+    /*
+    BOSS MONSTER or ELITE MONSTER
+    Skills:
+        Frozen Touch: Every hit from C. will freeze player for a second
+        Rime of Winter: Makes the weather to snow and snow deals damage
+        Cold Blood:
+        Rime of Frost: Spell that Cryomancer casts once a minute, it will freeze everyone near 20blocks of her
+            and deal them continuous damage for 3seconds of 30HP per hit
+                - will appear as white particles
+        Mark of Frost:
+        Cold Snap: Spell that Cryomancer casts once per 10-20 seconds, it will make a wave that deals 15HP, however it doesnt go through blocks
+        Glacial Wreath: Once the Cryomancer has 20% HP left this skill will be activated, it will make her invulnerable for 15 seconds while casting
+        Frozen Minions: Summons the Ice Golems 2x that will be her minions
+     */
+    public static void spawnCryomancer(Location spawnLoc){
+
+    }
+}
+
+
